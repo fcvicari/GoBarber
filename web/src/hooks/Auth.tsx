4ed04530798bc27a610ecad51b498e890e98e4c8
Fernando Cezar Vicari <fcvicari@gmail.com>
2020-05-04 21:28:10 -0300

    Inclusão da navegação em rotas autenticadas.

--- web/src/hooks/Auth.tsx
+++ web/src/hooks/Auth.tsx
@@ -22,7 +22,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 export const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>(() => {
     const token = localStorage.getItem('@GoBarber:token');
-    const user = localStorage.getItem('@GoBarber:use');
+    const user = localStorage.getItem('@GoBarber:user');
 
     if (token && user) {
       return { token, user: JSON.parse(user) };
