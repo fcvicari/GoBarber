382a5a3aabda22463fd4ecabb21473cb48c62407
Fernando Cezar Vicari <fcvicari@gmail.com>
2020-05-19 18:42:51 -0300

    Adicionado o controle das horas vagas em um determinado dia.

--- backend/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ backend/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -23,6 +23,20 @@ describe('ListProviderDayAvailability', () => {
       date: new Date(2020, 4, 20, 10, 0, 0),
     });
 
+    fakeAppointmentsRepository.create({
+      provider_id: 'providerOne',
+      date: new Date(2020, 4, 20, 14, 0, 0),
+    });
+
+    fakeAppointmentsRepository.create({
+      provider_id: 'providerOne',
+      date: new Date(2020, 4, 20, 15, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 20, 11).getTime();
+    });
+
     const availability = await listProviderDayAvailabilityService.execute({
       provider_id: 'providerOne',
       year: 2020,
@@ -33,9 +47,10 @@ describe('ListProviderDayAvailability', () => {
     expect(availability).toEqual(
       expect.arrayContaining([
         { hour: 8, available: false },
-        { hour: 9, available: true },
-        { hour: 10, available: false },
-        { hour: 11, available: true },
+        { hour: 9, available: false },
+        { hour: 11, available: false },
+        { hour: 14, available: false },
+        { hour: 16, available: true },
       ]),
     );
   });
